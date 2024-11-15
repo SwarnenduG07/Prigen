7cfe858cb194f2931d80083a91fd0eb0101e96ff
sunny <swarnendughosh05@gmail.com>
2024-11-15 21:27:45 +0530

    db connectin in main init

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,44 @@
+use config::Config;
+use db::DbCLient;
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions};
+use tracing_subscriber::filter::LevelFilter;
+
 mod config;
 mod model;
 mod dtos;
 mod error;
 mod db;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, Clone)]
+
+pub struct AppState {
+    pub env: Config,
+    pub db_client: DbCLient
+}
+
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+    .with_max_level(LevelFilter::DEBUG)
+    .init();
+
+    dotenv().ok();
+
+    let config = Config::init();
+    let pool = match PgPoolOptions::new()
+    .max_connections(10)
+    .connect(&config.database_url)
+    .await {
+        Ok(pool) => {
+            println!("Connection to db is succesfull! 🤝");
+            pool
+        }
+        Err(err) => {
+            println!("Fialed to connect to the database: {:?}", err);
+            std::process::exit(1)
+        }
+    };
+
 }
