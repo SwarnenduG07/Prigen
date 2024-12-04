1e126d0f1773c52fa5fc9e986a0e2337526d3d02
sunny <swarnendughosh05@gmail.com>
2024-12-04 12:15:56 +0530

    update user name fn added

--- backend/src/handler/user.rs
+++ backend/src/handler/user.rs
@@ -19,5 +19,27 @@ pub async fn get_me(
 }           
 
 pub async fn update_user_name(
-    
-)
+    Extension(_app_state): Extension<Arc<AppState>>,
+    Extension(user): Extension<JWTAuthMiddeware>,
+    Json(body): Json<NameUpdateDto>
+) -> Result<impl IntoResponse, HttpError> {
+     body.Validate()
+     .map_err(|e| HttpError::bad_request(e.to_string()))?;
+
+     let user = &user.user;
+     let user_id = uuid:Uuid::parse_str(&user.id.to_string()).unwrap();
+
+
+     let result = app_state.db_client
+                    .update_user_name(user_id.clone(), &body.name)
+                    .await
+                    .map_err(|e| HttpError::server_error(e.to_string()))?;
+
+                    let filter_user = FilterUserDto::filter_user(&result);
+
+                    let response = UserResponseDto {
+                        status: "success".to_string(),
+                        data: UserData {user: filter_user},
+                    };
+                    Ok(Json(response))
+}
